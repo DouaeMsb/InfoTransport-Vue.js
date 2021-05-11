dc8b7379d42195968573bba6d15846c046695cd5
Planet Computer <douaemsb99@gmail.com>
2021-05-11 03:57:46 +0100

    Create the Header & Sections that repeate.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <app-bar></app-bar>
     <v-main>
-      <router-view />
+      <router-view></router-view>
     </v-main>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title class="light-blue darken-4">
+          <strong class="subheading"> Get connected with us on social networks! </strong>
+          <v-spacer></v-spacer>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+            <v-icon size="24px">
+              {{icon}}
+            </v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-text class="py-2 white--text text-center">
+          {{new Date().getFullYear()}}-
+          <strong>&copy;InfoTransport.ma</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
-</template>
+  </template>
 
 <script>
+import AppBar from "./components/AppBar.vue";
 export default {
   name: "App",
-
+  components: { 
+   AppBar 
+},
   data: () => ({
-    //
-  }),
+ icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"]  }),
 };
 </script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
-  <hello-world />
+<div class="home">
+  <my-section background-image= "/images/transport.PNG">
+  <v-img src="/images/Logo.png" contain class="ma-auto mb-1" height="130px" width="250px"></v-img>
+  <v-card width ="40%" class="ma-auto mb-16" height="50px">
+    <v-text-field placeholder="search..." color="white" class="input d-flex ma-auto"></v-text-field>
+  </v-card>
+  </my-section>  
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import MySection from "../components/MySection.vue";
 
 export default {
   name: "Home",
-
   components: {
-    HelloWorld,
-  },
+    MySection
+  }
 };
 </script>
